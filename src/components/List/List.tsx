a1cc82f9af59af2cd19e6c582c91606caac32819
이진혁 <iih907@mediazen.co.kr>
2024-07-16 14:20:35 +0900

    ActionButton 컴포넌트 생성하기

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -67,7 +67,10 @@ const List: FC<TListProps> = ({
                     />
                 </div>
             ))}
-            <ActionButton />
+            <ActionButton
+                boardId={boardId}
+                listId={list.listId}
+            />
         </div>
     )
 }
